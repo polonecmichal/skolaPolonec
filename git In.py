65e8c48c69b9345858a0d8f4acb5789355c79862
polonecmichal <polonecmichal36@gmail.com>
2025-09-17 08:36:36 +0200

    volna

---
+++ git In.py
@@ -0,0 +1,7 @@
+echo "# skola" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/polonecmichal/skola.git
+git push -u origin main
